2ac19119cfe323ad1eba9d07310fb0a6a927fdb6
ackentdev <adamkentdev@gmail.com>
2020-02-20 11:54:26 -0700

    added auth endpoints

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const massive = require('massive');
 const session = require('express-session');
 // Import variables
 const {SERVER_PORT, SESSION_SECRET, CONNECTION_STRING} = process.env
+const {login, register, logout, userSession} = require('./controller/authCtrl')
 // top-level middleware
 const app = express();
 app.use(express.json());
@@ -26,6 +27,13 @@ massive(CONNECTION_STRING).then( db => {
 }).catch( err => console.log(err))
 
 // endpoints
+// AUTH
+app.post('/auth/login', login);
+app.post('/auth/register', register);
+app.get('/auth/userSession', userSession);
+app.get('/api/logout', logout);
+// MEMES
+
 
 // run server on our port
 app.listen(SERVER_PORT, () => console.log(`Say hello to my little ${SERVER_PORT}`))
